e772508f33b85fbb22cff2e06abe6069e03ac579
Sleen123 <152021855+Sleen123@users.noreply.github.com>
2023-11-25 20:04:33 +0300

    All my fellas
    LOL

---
+++ Boom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Boooooooooooooooom
+        Destroy(gameObject);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
